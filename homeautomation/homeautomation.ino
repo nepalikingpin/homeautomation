914f2a9c2c3e9b07cd2b9fdc396de5c9f29c0841
Manish Bhattarai <bhaimanish00@gmail.com>
2020-12-26 22:28:51 +0545

    Added motion detector light

--- homeautomation/homeautomation.ino
+++ homeautomation/homeautomation.ino
@@ -1,32 +1,67 @@
-#include "DHT.h"
+//#include "DHT.h"
 
-#define DHTPIN 11
-#define DHTTYPE DHT22
+//#define DHTPIN 11
+//#define DHTTYPE DHT22
 
-DHT dht(DHTPIN, DHTTYPE);
+const int trigPin = 7;
+const int echoPin = 6;
+int relayPin = 12;
+
+// defines variables
+long duration;
+int distance;
+
+//DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600);
-  dht.begin();
+//  dht.begin();
+  pinMode(relayPin, OUTPUT);
 }
 
 void loop() {
 
-  delay(2000);
+//  delay(2000);
   
-  float h =dht.readHumidity();
+//  float h =dht.readHumidity();
+//
+//  float t = dht.readTemperature();
+//
+//  if (isnan(h) || isnan(t)) {
+//    Serial.println("Failed to read from DHT sensor!");
+//    return;
+//  }
+//
+//  Serial.print("Humidity: "); 
+//  Serial.print(h);
+//  Serial.print(" %\t");
+//  Serial.print("Temperature: "); 
+//  Serial.print(t);
+//  Serial.println(" *C ");
 
-  float t = dht.readTemperature();
+  // Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
 
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
 
-  Serial.print("Humidity: "); 
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: "); 
-  Serial.print(t);
-  Serial.println(" *C ");
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+distance= duration*0.034/2;
+delay(100);
+Serial.println(distance);
+if(distance <20) {
+  digitalWrite(relayPin, LOW);
+  delay(10);
+}
+else {
+  digitalWrite(relayPin, HIGH);
+}
 }
